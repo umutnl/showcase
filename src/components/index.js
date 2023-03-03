5ad99e4ce89b356f6e9951a00edff8cacc316bf2
umutnl <96056187+umutnl@users.noreply.github.com>
2023-03-03 14:09:24 +0100

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+// bu index dosyasinin icine componentleri koyuyoruz daha kolay yonetmek icin App dosyasinin icine koyarsak cok fazla kod gorunmus olacak ve karisik gozukecek
+
+
+import Features from './Features'
+
+import Download from './Download'
+
+import SectionWrapper from './SectionWrapper'
+
+
+
+export {
+
+
+    Features, Download, SectionWrapper
+}
